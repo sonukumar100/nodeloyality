e34ccc2a3a22fa27b31dbbeca6e9c656856bc48c
sonuaura <sonukuma7840871141@gmail.com>
2025-04-10 23:44:14 +0530

    connectsql

--- src/db/index.js
+++ src/db/index.js
@@ -2,17 +2,27 @@
   import fs from 'fs';
   // const mysql = require("mysql2/promise");
   import mysql from 'mysql2/promise';
+import path from 'path';
+import dotenv from 'dotenv';
+import { fileURLToPath } from 'url';
+
+// Fix __dirname for ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Load .env from the root directory
+dotenv.config({ path: path.join(__dirname, '../.env') });
 
 
   // Create a MariaDB connection pool
-
+console.log("Connecting to MariaDB...",process.env.DB_HOST);
 
   // Test connection once at startup
   export const pool = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "loyalty",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD || "",
+    database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 20,
     queueLimit: 0,
